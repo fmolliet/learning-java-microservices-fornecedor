9c143d7c34763c6f406c20accf709b86811e655c
fmolliet <fmolliet@hotmail.com>
2021-05-05 23:36:26 -0300

    Adicionado autentificação no microserviço

--- src/main/java/br/com/alura/microservico/fornecedor/FornecedorApplication.java
+++ src/main/java/br/com/alura/microservico/fornecedor/FornecedorApplication.java
@@ -3,9 +3,11 @@ package br.com.alura.microservico.fornecedor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableResourceServer
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
